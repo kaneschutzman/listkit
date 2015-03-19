f720e881dcfd4388c326b6bbc28fbcc8864ee531
Alexander di Chiara <alexander.dichiara@gmail.com>
2015-03-19 21:30:49 +0100

    push first

--- api/models/session.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var sessionSchema   = mongoose.Schema({
-    sessionActivity	:    { type: Date, expires: '172800s', default: Date.now },
-    user_token		:    { type: String, required: true },
-    user_id 		:    { type: String, required: true }
-});
-
-module.exports = mongoose.model('Session', sessionSchema);
